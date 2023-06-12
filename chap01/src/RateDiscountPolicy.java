20042a7dfc3178145bcd6bfe2b609eb25259d538
seongjikang <java7ang@gmail.com>
2023-06-12 22:05:02 +0900

    CHAPTER 01

---
+++ chap01/src/RateDiscountPolicy.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+    private int discountPercent = 10;
+    @Override
+    public void discount(List<Product> products) {
+        for(Product product : products) {
+            product.setPrice(product.getPrice() - (product.getPrice() * discountPercent / 100));
+        }
+    }
+}
